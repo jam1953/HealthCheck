16e0655382d18b9e7dc8de8b3d3ca7d294c36d5d
sdh2110 <sdh2110@g.rit.edu>
2018-10-16 18:49:11 -0400

    Adding the medication reminder screen.

---
+++ app/src/main/java/com/example/lenovo/healthcheck/activity_steven_2.java
@@ -0,0 +1,31 @@
+package com.example.lenovo.healthcheck;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class activity_steven_2 extends AppCompatActivity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steven_2);
+
+        button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
